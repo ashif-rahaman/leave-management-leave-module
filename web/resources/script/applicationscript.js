7d0f76d334754ef2db7fe01fe2001c97f3060d6e
ashif-rahaman <ashifrahamanuis@gmail.com>
2017-12-30 16:14:07 +0600

    Done: Application form validation is done.

--- web/resources/script/applicationscript.js
+++ web/resources/script/applicationscript.js
@@ -1,2 +1,59 @@
+var subject = false;
+var body = false;
+
+/**
+ * 
+ * @param {type} subjectValue
+ * @returns {undefined}
+ */
+var bodyValidator = function (bodyValue) {
+
+    bodyValue = bodyValue.trim();
+    var bodyField = document.getElementById('apply_body');
+
+    if (bodyValue.length !== 0) {
+
+        bodyField.style.borderWidth = '2px';
+        bodyField.style.borderColor = '#4ef442';
+    } else {
+
+        bodyField.style.borderWidth = '2px';
+        bodyField.style.borderColor = '#fc0000';
+    }
+};
 
 
+/**
+ * 
+ * @param {type} subjectValue
+ * @returns {undefined}
+ */
+var subjectValidator = function (subjectValue) {
+
+    subjectValue = subjectValue.trim();
+    var subjectField = document.getElementById('apply_subject');
+
+    if (subjectValue.length !== 0) {
+
+        subjectField.style.borderWidth = '2px';
+        subjectField.style.borderColor = '#4ef442';
+    } else {
+
+        subjectField.style.borderWidth = '2px';
+        subjectField.style.borderColor = '#fc0000';
+    }
+};
+
+/**
+ * 
+ * @returns {Boolean}
+ */
+var applicationInsertValidator = function () {
+
+    if (subject && body) {
+
+        return true;
+    }
+
+    return false;
+};
